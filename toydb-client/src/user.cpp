143d4c14f20e9293dc999db9a3be8fcb3fd00705
koltday <33640519+koltday@users.noreply.github.com>
2023-05-04 11:19:14 -0500

    Deleting extra directories

--- toydb-client/src/user.cpp
+++
@@ -1,7 +0,0 @@
-#include "../include/user.h"
-
-namespace ToyDB {
-std::string ToyDB::User::get_username() { return this->username; }
-
-std::string ToyDB::User::get_password() { return this->password; }
-} // namespace ToyDB
--- toydb-server/src/toydb-client.cpp
+++
@@ -1,7 +0,0 @@
-#include "../include/networking.h"
-#include <iostream>
-#include <string>
-
-int main() {
-  ToyDB::ClientSocket *client = new ToyDB::ClientSocket("127.0.0.1", 1028);
-}
